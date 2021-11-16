a2ad722d591bbd5ad218be40e07115387cc7c504
Sham <shamthakare1992@gmail.com>
2021-11-16 09:32:22 +0530

    DB CAll Changes

--- db.js
+++ db.js
@@ -1,44 +1,38 @@
-const mongoose= require ('mongoose');
-const employee = require('./models/employee');
-const router = require('./routes/routes');
+const mongoose = require("mongoose");
+const employee = require("./models/employee");
+const router = require("./routes/routes");
 
-mongoose.connect('mongodb://localhost:27017/meanDB',(err)=>{
-    if(!err){
-        console.log(' DB connection successful..');
-
-    }else{
-        console.log('Error in connection' + err);
-
-    }
-
-})
+mongoose.connect("mongodb://localhost:27017/meanDB", (err) => {
+  if (!err) {
+    console.log(" DB connection successful..");
+  } else {
+    console.log("Error in connection" + err);
+  }
+});
 // get Api
-router.get('/',(req,res)=>{
-    employee.find((err,doc)=>{
-        if(err){
-            console.log('err in get data'+err);
-        }else{
-            res.send(doc);
-        }
-    })
+router.get("/", (req, res) => {
+  employee.find((err, doc) => {
+    if (err) {
+      console.log("err in get data" + err);
+    } else {
+      res.send(doc);
+    }
+  });
 });
 // post Api
-router.post('/',(req,res)=>{
-    let emp = new employee({
-        name: req.body.name,
-        position:req.body.position,
-        sept:req.body.dept
-    });
-    emp.save(err,doc)=>{
-        if(err){
-            console.log('err in post data'+err);
-        }else{
-            res.send(doc);
-        }
+router.post("/", (req, res) => {
+  let emp = new employee({
+    name: req.body.name,
+    position: req.body.position,
+    sept: req.body.dept,
+  });
+  emp.save((err, doc) => {
+    if (err) {
+      console.log("err in post data" + err);
+    } else {
+      res.send(doc);
     }
-})
-
-
-
+  });
+});
 
 module.exports = mongoose;
